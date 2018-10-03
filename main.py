192f3c282ab04fee87207f9bdc4761bc1759ba7a
Bielrv <rosinol.gabriel@gmail.com>
2018-10-03 11:33:02 +0200

    Create data class

--- main.py
+++ main.py
@@ -1,6 +1,25 @@
-from google_trends import trend
+import pandas as pd
+import datetime
+from functions import dataframe_to_csv
+
+class data():
+
+    def __init__(self):
+
+        from google_trends import trend
+        trends = trend(["MasMovil", "Movistar", "Vodafone", "Orange"])
+        dataframe_to_csv('trends', trends.df)
 
 
-trends = trend(["MasMovil", "Movistar", "Vodafone", "Orange"])
+        from alpha_api import alpha_stock
+        stocks = alpha_stock()
+        dataframe_to_csv('stocks', stocks.df)
 
-trends.df
+        complete_df = trends.df.join(stocks.df)
+        complete_df = complete_df.drop(columns='isPartial')
+
+        self.df = complete_df
+
+# from alphavantage import stock
+# stocks = stock('ORTH.ILN')
+# stocks.df
