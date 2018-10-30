ec022e926a83d8814e31214eae022f9e8ed9d9ba
Bielrv <rosinol.gabriel@gmail.com>
2018-10-30 16:11:48 +0100

    Organize directories

--- main.py
+++ main.py
@@ -2,16 +2,16 @@ import pandas as pd
 import datetime
 from functions import dataframe_to_csv
 
+
 class data():
 
     def __init__(self):
 
-        from google_trends import trend
+        from modules.google_trends import trend
         trends = trend(["MasMovil", "Movistar", "Vodafone", "Orange"])
         dataframe_to_csv('trends', trends.df)
 
-
-        from alpha_api import alpha_stock
+        from modules.alpha_api import alpha_stock
         stocks = alpha_stock()
         stocks.df.shape
         dataframe_to_csv('stocks', stocks.df)
@@ -21,6 +21,5 @@ class data():
 
         self.df = complete_df
 
-# from alphavantage import stock
-# stocks = stock('ORTH.ILN')
-# stocks.df
+
+data = data()
--- modules/downloader.py
+++ modules/downloader.py
@@ -7,4 +7,9 @@ headers = {}
 r = requests.get("https://trends.google.com/trends/explore?date=now%201-H&geo=ES&q=masmovil,movistar,vodafone,orange", headers=headers)
 # parse and retrieve two vital form values
 if not r.status_code == 200:
-    print('Error:'+str(r.status_code))
+    print('Error:'+ str(r.status_code))
+
+r = requests.get("https://apps.tampagov.net/CallsForService_Webapp/Default.aspx?type=TPD", headers=headers)
+# parse and retrieve two vital form values
+if not r.status_code == 200:
+    print('Error'+ str(r.status_code))
